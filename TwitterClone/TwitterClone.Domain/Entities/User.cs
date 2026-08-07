ab48dd6190bf009c688e63fc58c27f59a7dafc83
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2026-08-07 22:56:20 +0600

    Initial commit

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _password;
+        private string _gender;
+        private string _phone;
+
+        public User()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+    }
+}
